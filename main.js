e327e801ea1cb17b06a9d5d4b3a4eed1ccd8ff07
Ijaz3Ahmed <146079789+Ijaz3Ahmed@users.noreply.github.com>
2023-09-27 17:59:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+   function IndianCricketers(){ 
+    if(true) {
+        var a ="dhoni"
+        document.getElementById("player1").textContent=a ;
+    }
+        }
+
+
+IndianCricketers();
+    
+    function IndianCricketers1() {
+        
+    if(true) {
+            let b="team"
+        document.getElementById("player2").textContent=b;
+        
+          }
+        }
+IndianCricketerers1();
+
+function IndianCricketers2(){
+    if(true){
+    const c="kholi"
+    document.getElementById("player3").textContent=c;
+        
+}
+}
+IndianCricketers2();
